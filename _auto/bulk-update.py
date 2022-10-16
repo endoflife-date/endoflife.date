0259e09c67abfde72f1d681d3b2f77e5728b6adf
Nemo <commits@captnemo.in>
2022-10-16 14:30:34 +0530

    Fix validation and style
    Validates several aspects, including category and types of
    various fields

--- _auto/bulk-update.py
+++ _auto/bulk-update.py
@@ -18,41 +18,47 @@ DEFAULT_POST_TEMPLATE = """\
 {content}
 """
 
+
 def yaml_to_str(obj):
-  yaml = YAML()
-  yaml.indent(sequence=4)
-  string_stream = StringIO()
-  yaml.dump(obj, string_stream)
-  output_str = string_stream.getvalue()
-  string_stream.close()
-  return output_str
+    yaml = YAML()
+    yaml.indent(sequence=4)
+    string_stream = StringIO()
+    yaml.dump(obj, string_stream)
+    output_str = string_stream.getvalue()
+    string_stream.close()
+    return output_str
+
 
 """
 Takes a product dict
 and return the updated version
 """
+
+
 def update(obj):
-  return obj
+    return obj
+
 
 def update_product(name):
-  fn = 'products/%s.md' % name
-  print(name)
-  with open(fn, 'r+') as f:
-    yaml = YAML()
-    yaml.preserve_quotes = True
-    data = next(yaml.load_all(f))
-    f.seek(0)
-    _, content = frontmatter.parse(f.read())
+    fn = "products/%s.md" % name
+    print(name)
+    with open(fn, "r+") as f:
+        yaml = YAML()
+        yaml.preserve_quotes = True
+        data = next(yaml.load_all(f))
+        f.seek(0)
+        _, content = frontmatter.parse(f.read())
 
-    data = update(data)
-    final_contents = DEFAULT_POST_TEMPLATE.format(
-      metadata=yaml_to_str(data),
-      content=content)
+        data = update(data)
+        final_contents = DEFAULT_POST_TEMPLATE.format(
+            metadata=yaml_to_str(data), content=content
+        )
 
-    f.seek(0)
-    f.truncate()
-    f.write(final_contents)
+        f.seek(0)
+        f.truncate()
+        f.write(final_contents)
 
-if __name__ == '__main__':
-  for x in glob('products/*.md'):
-    update_product(Path(x).stem)
+
+if __name__ == "__main__":
+    for x in glob("products/*.md"):
+        update_product(Path(x).stem)
--- _auto/latest.py
+++ _auto/latest.py
@@ -30,21 +30,21 @@ DEFAULT_POST_TEMPLATE = """\
 # https://stackoverflow.com/a/63092850/368328
 def sort_versions(data):
     def key(n):
-        a = re.split(r'(\d+)', n)
+        a = re.split(r"(\d+)", n)
         a[1::2] = map(int, a[1::2])
         return a
+
     return sorted(data, key=lambda n: key(n))
+
 
 # https://stackoverflow.com/a/71329221/368328
 # Force encoding version numbers as strings
 Resolver.add_implicit_resolver(
-    'tag:yaml.org,2002:string',
-    re.compile(r'\d+\.\d+\.\d+', re.X),
-    list('.0123456789'))
+    "tag:yaml.org,2002:string", re.compile(r"\d+\.\d+\.\d+", re.X), list(".0123456789")
+)
 Resolver.add_implicit_resolver(
-    'tag:yaml.org,2002:string',
-    re.compile(r'\d+\.\d+', re.X),
-    list('.0123456789'))
+    "tag:yaml.org,2002:string", re.compile(r"\d+\.\d+", re.X), list(".0123456789")
+)
 
 
 """
@@ -52,87 +52,101 @@ matches releases that are exact (such as 4.1 being the first release for the 4.1
 or releases that include a dot just after the release cycle (4.1.*)
 This is important to avoid edge cases like a 4.10.x release being marked under the 4.1 release cycle.
 """
+
+
 def releases_matches(r, prefix):
-  return (r.startswith(prefix) and (r == prefix or r.startswith(prefix + '.') or r.startswith(prefix + '-')))
+    return r.startswith(prefix) and (
+        r == prefix or r.startswith(prefix + ".") or r.startswith(prefix + "-")
+    )
+
 
 def find_first(releases, prefix):
-  return next(filter(lambda r: releases_matches(r, prefix), releases), None)
+    return next(filter(lambda r: releases_matches(r, prefix), releases), None)
+
 
 def find_last(releases, prefix):
-  return next(filter(lambda r: releases_matches(r, prefix), reversed(releases)), None)
+    return next(filter(lambda r: releases_matches(r, prefix), reversed(releases)), None)
+
 
 def find_all(releases, prefix):
-  return set(filter(lambda r: releases_matches(r, prefix), releases))
+    return set(filter(lambda r: releases_matches(r, prefix), releases))
+
 
 def yaml_to_str(obj):
-  yaml = YAML()
-  yaml.indent(sequence=4)
-  string_stream = StringIO()
-  yaml.dump(obj, string_stream)
-  output_str = string_stream.getvalue()
-  string_stream.close()
-  return output_str
+    yaml = YAML()
+    yaml.indent(sequence=4)
+    string_stream = StringIO()
+    yaml.dump(obj, string_stream)
+    output_str = string_stream.getvalue()
+    string_stream.close()
+    return output_str
+
 
 def update_product(name):
-  fn = 'products/%s.md' % name
-  with open(fn, 'r+') as f:
-    yaml = YAML()
-    yaml.preserve_quotes = True
-    data = next(yaml.load_all(f))
-
-    f.seek(0)
-    _, content = frontmatter.parse(f.read())
-
-    fn = '_data/release-data/releases/%s.json' % (name)
-    if exists(fn):
-      with open(fn) as releases_file:
-        # Entire releases data as a dict
-        R1 = json.loads(releases_file.read())
-        # Just the list of versions
-        R2 = sort_versions(R1.keys())
-        R3 = set(R2)
-
-      updated = False
-
-      for release in data['releases']:
-        old = release.copy()
-
-        prefix = release['releaseCycle']
-        first_version = find_first(R2, prefix)
-        latest_version = find_last(R2, prefix)
-
-        R3 = R3 - find_all(R3, prefix)
-
-        if first_version:
-          release['releaseDate'] = datetime.date.fromisoformat(R1[first_version])
-          release['latestReleaseDate'] = datetime.date.fromisoformat(R1[latest_version])
-          release['latest'] = latest_version
-          diff = DeepDiff(old, release, ignore_order=True)
-
-          # We write back to the file
-          if(diff!={}):
-            updated = True
-
-      if updated:
-        print("Updating %s" % fn)
-        final_contents = DEFAULT_POST_TEMPLATE.format(
-          metadata=yaml_to_str(data),
-          content=content)
+    fn = "products/%s.md" % name
+    with open(fn, "r+") as f:
+        yaml = YAML()
+        yaml.preserve_quotes = True
+        data = next(yaml.load_all(f))
 
         f.seek(0)
-        f.truncate()
-        f.write(final_contents)
+        _, content = frontmatter.parse(f.read())
 
-      if len(R3) != 0:
-        for x in R3:
-          date = datetime.date.fromisoformat(R1[x])
-          days_since_release = (datetime.date.today() - date).days
-          if days_since_release < 30:
-            print("[WARN] %s:%s (%s) not included" % (name, x, R1[x]))
+        fn = "_data/release-data/releases/%s.json" % (name)
+        if exists(fn):
+            with open(fn) as releases_file:
+                # Entire releases data as a dict
+                R1 = json.loads(releases_file.read())
+                # Just the list of versions
+                R2 = sort_versions(R1.keys())
+                R3 = set(R2)
 
-if __name__ == '__main__':
-  if len(sys.argv) > 1:
-    update_product(sys.argv[1])
-  else:
-    for x in glob('products/*.md'):
-      update_product(Path(x).stem)
+            updated = False
+
+            for release in data["releases"]:
+                old = release.copy()
+
+                prefix = release["releaseCycle"]
+                first_version = find_first(R2, prefix)
+                latest_version = find_last(R2, prefix)
+
+                R3 = R3 - find_all(R3, prefix)
+
+                if first_version:
+                    release["releaseDate"] = datetime.date.fromisoformat(
+                        R1[first_version]
+                    )
+                    release["latestReleaseDate"] = datetime.date.fromisoformat(
+                        R1[latest_version]
+                    )
+                    release["latest"] = latest_version
+                    diff = DeepDiff(old, release, ignore_order=True)
+
+                    # We write back to the file
+                    if diff != {}:
+                        updated = True
+
+            if updated:
+                print("Updating %s" % fn)
+                final_contents = DEFAULT_POST_TEMPLATE.format(
+                    metadata=yaml_to_str(data), content=content
+                )
+
+                f.seek(0)
+                f.truncate()
+                f.write(final_contents)
+
+            if len(R3) != 0:
+                for x in R3:
+                    date = datetime.date.fromisoformat(R1[x])
+                    days_since_release = (datetime.date.today() - date).days
+                    if days_since_release < 30:
+                        print("[WARN] %s:%s (%s) not included" % (name, x, R1[x]))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        update_product(sys.argv[1])
+    else:
+        for x in glob("products/*.md"):
+            update_product(Path(x).stem)
