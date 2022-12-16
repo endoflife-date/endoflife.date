c22dcea411d850fd506e3ef7ad364a1ff310166f
Nemo <commits@captnemo.in>
2022-12-16 18:35:38 +0530

    [design] Move product description to the top (#2069)
    So the layout becomes:
    
    - Description
    - Table
    - Policy
    
    Fixes #2065

---
+++ _plugins/jekyll-element-filters.rb
@@ -0,0 +1,28 @@
+require 'nokogiri'
+
+module LiquidFilter
+  def extract_element(html, element)
+    entries = []
+    @doc = Nokogiri::HTML::DocumentFragment.parse(html)
+
+    @doc.css(element).each do |node|
+      entries << node.to_html
+    end
+    entries
+  end
+
+  # Removes the first element of the
+  # kind from the HTML
+  def remove_first_element(html, element)
+    doc = Nokogiri::HTML::DocumentFragment.parse(html)
+    e = doc.css(element)
+    e.first.remove if e && e.first
+    doc.to_html
+  end
+end
+
+Liquid::Template.register_filter(LiquidFilter)
+
+
+
+
