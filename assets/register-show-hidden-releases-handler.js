8f1caa9207387cf1d949ab49ab55c803eaa9e4a2
Marc Wrobel <marc.wrobel@gmail.com>
2023-03-22 12:06:35 +0100

    Hide oldest unmaintained releases cycles using javascript (#50)
    I don't know if it makes things more accessible, but at least people that disabled JavaScript on their browser can now see all release cycles.
    
    A small glitch can sometime be observed on page load (for example on API Platform page) but that's not systematic and I didn't consider that to be an issue.

--- assets/register-show-hidden-releases-handler.js
+++ assets/register-show-hidden-releases-handler.js
@@ -1,12 +1,23 @@
+function showHideOldReleases(show) {
+  const showMoreRow = document.getElementById("show-more-row");
+  const releases = document.querySelectorAll(".release.can-be-hidden");
+
+  if(show) {
+    showMoreRow.classList.add('d-none');
+    releases.forEach((r) => r.classList.remove('d-none'));
+  } else {
+    showMoreRow.classList.remove('d-none');
+    releases.forEach((r) => r.classList.add('d-none'));
+  }
+}
+
 function registerShowHiddenReleasesHandler() {
   document.getElementById('show-hidden-releases-button').addEventListener("click",
     (event) => {
       event.preventDefault();
-      document.getElementById("show-more-row").classList.add('d-none');
-      document.querySelectorAll(".release.d-none").forEach(
-        (c) => c.classList.remove('d-none')
-      );
+      showHideOldReleases(true);
     }, false);
 }
 
+showHideOldReleases(false);
 registerShowHiddenReleasesHandler();
