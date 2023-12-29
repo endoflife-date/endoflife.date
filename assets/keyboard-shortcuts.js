b27da9995d06a2e5c158cfe47658c4e7cabb5dc1
Roman KC <roman@bajratechnologies.com>
2023-12-29 15:46:57 +0545

    doc: update method documentation for clarity

--- assets/keyboard-shortcuts.js
+++ assets/keyboard-shortcuts.js
@@ -1,6 +1,7 @@
 /**
  * Handles Ctrl + K keypress to focus on a specified search input element
- * SEE: https://github.com/just-the-docs/just-the-docs/blob/main/_includes/components/search_header.html
+ * Input element ID retrieved from
+ * https://github.com/just-the-docs/just-the-docs/blob/main/_includes/components/search_header.html
  */
 document.addEventListener("DOMContentLoaded", function () {
   document.addEventListener("keydown", function (e) {
